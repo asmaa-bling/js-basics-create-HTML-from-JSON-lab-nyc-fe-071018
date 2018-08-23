455ec3798c6c8b0129696ea58f3ffc900f9fe31a
Asmaa Abbas <asmaa@flatironschool.com>
2018-08-23 23:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,17 +12,22 @@ document.addEventListener("DOMContentLoaded", (e) => {
   });
 });
 
-function updateHTMLContent() {
+function updateHTMLContent(movieName) {
+  let currentMovie = movies[movieName];
   let title = document.getElementById("title");
   let director = document.getElementById("director");
   let genre = document.getElementById("genre");
   let filmRating = document.getElementById("filmRating");
   let poster = document.getElementById("poster");
+  let audienceScore = document.getElementById("audienceScore");
+  let description = document.getElementById("description");
   
-  title.innerHTML = movies["Titanic"].title;
-  director.innerHTML = movies["Titanic"].director;
-  genre.innerHTML = movies["Titanic"].genre;
-  filmRating.innerHTML = movies["Titanic"].filmRating;
-  poster.setAttribute('src', movies["Titanic"].poster);
+  title.innerHTML = movies[currentMovie].title;
+  director.innerHTML = movies[currentMovie].director;
+  genre.innerHTML = movies[currentMovie].genre;
+  filmRating.innerHTML = movies[currentMovie].filmRating;
+  poster.setAttribute('src', movies[currentMovie].poster);
+  description.innerHTML = movies[currentMovie].description;
+  audienceScore.innerHTML = movies[currentMovie].audienceScore;
 }
 
