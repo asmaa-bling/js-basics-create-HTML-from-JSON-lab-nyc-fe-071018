1f29539c84326a145deca68c50971660b6bdd759
Asmaa Abbas <asmaa@flatironschool.com>
2018-08-23 23:27:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
+  const titanicButton = document.getElementById('Titanic');
+  const terminatorButton = document.getElementById('Terminator 2');
   
+  titanicButton.addEventListener('click', (event) => {
+    
+  });
+  
+  terminatorButton.addEventListener('click', (event) => {
+    
+  });
 });
